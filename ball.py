e921073e56fa7ccc4b8ba1cef46533f1b8a3b467
dilan saskin <dilansskn@gmail.com>
2023-10-04 22:16:35 +0300

    accelerate the ball when it touches paddles

--- ball.py
+++ ball.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-
+STARTING_SPEED=8
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
@@ -9,8 +9,8 @@ class Ball(Turtle):
         self.shapesize(stretch_len=0.7, stretch_wid=0.7)
         self.color("white")
         self.speed("fastest")
-        self.move_x = 10
-        self.move_y = 10
+        self.move_x = STARTING_SPEED
+        self.move_y = STARTING_SPEED
 
     def moveBall(self):
         new_y = self.ycor() +self.move_y
@@ -25,4 +25,10 @@ class Ball(Turtle):
     def miss(self):
         self.home()
         self.bounce_x()
+        self.move_x = STARTING_SPEED
+        self.move_y = STARTING_SPEED
 
+
+    def accelerate(self):
+        self.move_x *= 1.1
+        self.move_y *= 1.1
